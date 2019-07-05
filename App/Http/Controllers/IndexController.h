bedb22a8e8fb2c2929375784ae6c36b1048d6ed2
Pedro Soares <contato@pedrosoares.net>
2019-07-05 11:30:32 -0300

    Using docker with auto-build. Updated SQLite Driver Quotes.

--- App/Http/Controllers/IndexController.h
+++ App/Http/Controllers/IndexController.h
@@ -10,8 +10,8 @@ class IndexController : public Controller {
 
     public:
         Response index(){
-            DataObject conn("pgsql:host=172.18.0.2 port=5432 dbname=postgres", "postgres", "1234");
-            //DataObject conn("sqlite:sqlite_database.sqlite");
+            //DataObject conn("pgsql:host=172.18.0.2 port=5432 dbname=postgres", "postgres", "1234");
+            DataObject conn("sqlite:sqlite_database.sqlite");
 
             //auto result = conn.prepare("select * from users;");
             //result->execute();
@@ -25,9 +25,10 @@ class IndexController : public Controller {
             //auto result = conn.prepare("select * from users where email = ? and password = ?;");
             //result->execute(std::vector<Value>({"jhon@due.com", "5555"}));
 
+            int res = 0;
 
             //int res = conn.exec(R"(CREATE TABLE IF NOT EXISTS `users` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT, `email` TEXT, `password` TEXT );)");
-            int res = conn.exec(R"(CREATE TABLE IF NOT EXISTS "users" ( "id" SERIAL PRIMARY KEY, "name" character varying, "email" character varying, "password" character varying );)");
+            //int res = conn.exec(R"(CREATE TABLE IF NOT EXISTS "users" ( "id" SERIAL PRIMARY KEY, "name" character varying, "email" character varying, "password" character varying );)");
             //int res = conn.exec(R"(insert into users (name, email, password) values ("a", "a", "a");)");
             //int res = conn.exec(R"(delete from users where name = "a";)");
 
@@ -38,9 +39,12 @@ class IndexController : public Controller {
             conn.beginTransaction();
             auto a = conn.prepare("insert into users (name, email, password) values (?, ?, ?);");
             a->execute({"a", "a", "a"});
-            conn.exec("insert into users (name, email, password) values ('b', 'b', 'b');");
-            conn.exec("insert into users (name, email, password) values ('c', 'c', 'c');");
-            /*conn.commit();*/
+            a->fetchAll();
+            //conn.exec("insert into users (name, email, password) values ('b', 'b', 'b');");
+            //conn.exec("insert into users (name, email, password) values ('c', 'c', 'c');");
+            //conn.exec("delete from users where LENGTH(name) = 1 and name != 'a';");
+            //conn.commit();
+            conn.rollBack();
 
             auto result = conn.prepare("select * from users;");
             result->execute();
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,28 @@
 FROM httpd:2.4
 
-RUN apt-get update && apt-get install -y valgrind
+RUN apt-get update && apt-get install -y valgrind gcc g++ cmake inotify-tools
+
+RUN echo "@.idea" >> /tmp/lixo && \
+    echo "@.git" >> /tmp/lixo && \
+    echo "@cmake-build-debug" >> /tmp/lixo
+
+RUN echo "mkdir -p /usr/source/cmake-build-debug" >> /usr/local/bin/auto-build && \
+    echo "cd /usr/source/cmake-build-debug" >> /usr/local/bin/auto-build && \
+    echo "cmake .." >> /usr/local/bin/auto-build && \
+    echo "make -j4" >> /usr/local/bin/auto-build && \
+    echo "while true; do" >> /usr/local/bin/auto-build && \
+    echo "inotifywait -e modify,create,delete -r --fromfile /tmp/lixo /usr/source/ && make" >> /usr/local/bin/auto-build && \
+    #echo "inotifywait -e modify,create,delete -r /usr/source/ && make" >> /usr/local/bin/auto-build && \
+    echo "done" >> /usr/local/bin/auto-build && \
+    chmod 777 /usr/local/bin/auto-build
+
+RUN echo "rm -f /usr/local/apache2/logs/httpd.pid" >> /usr/local/bin/httpd-foreground && \
+    echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/apache2/htdocs/lib/" >> /usr/local/bin/httpd-foreground && \
+    echo "echo 1" >> /usr/local/bin/httpd-foreground && \
+    echo "auto-build &" >> /usr/local/bin/httpd-foreground && \
+    echo "echo 2" >> /usr/local/bin/httpd-foreground && \
+    echo "httpd -DFOREGROUND" >> /usr/local/bin/httpd-foreground && \
+    chmod 777 /usr/local/bin/httpd-foreground
+
 
 COPY ./httpd.conf /usr/local/apache2/conf/httpd.conf
-COPY httpd-foreground /usr/local/bin/
